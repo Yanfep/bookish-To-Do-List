f95cb6adf1cda4be1b63c964b07bf4e74082cefe
Yanfe Pedroza Alberto <38539546+Yanfep@users.noreply.github.com>
2023-07-16 20:51:37 +0000

    fix css

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -4,43 +4,43 @@ import React, { useState } from "react";
 export const ToDolist = () => {
     const [taskInput, setTaskInput] = useState('');
     const [taskList, setTaskList] = useState(["task1", "task2"]);
-
     const handleInputChange = (event) => {
         setTaskInput(event.target.value);
     };
 
-    function addTask(event){
+    function addTask(event) {
         if (event.key == "Enter") {
-          setTaskList([...taskList, taskInput]);
-          setTaskInput('');
+            setTaskList([...taskList, taskInput]);
+            setTaskInput('');
         }
-      };
+    };
 
-    const task = taskList.map(element =>{
+    const task = taskList.map(element => {
         return (
             <li key={element.id} id="task" className="list-group-item">{element}
-            <button type="button" id="button" className="btn-close" onClick={() => deleteTask(element)}></button> </li>
-
+                <button type="button" id="button" className="btn-close" onClick={() => deleteTask(element)}></button> </li>
         )
-      });
-    
+    });
 
-    function deleteTask (element) {
-            setTaskList(taskList.filter(item => item !== element)
-        )};
+    function deleteTask(element) {
+        setTaskList(taskList.filter(item => item !== element)
+        )
+    };
 
-	return (
-        <div className="d-flex justify-content-center">
+    return (
+        <>
             <div>
-                <input type="input" id="theInput"className="form-control" onChange={handleInputChange} value={taskInput} onKeyDown={addTask} placeholder="Enter a task"/>
+                <input type="input" id="theInput" className="form-control" onChange={handleInputChange} value={taskInput} onKeyDown={addTask} placeholder="Enter a task" />
             </div>
-            <ul className="list-group">
-                {task}
-            </ul>
-            <div className="task1 card card-body"><small class="text-body-secondary"> {taskList.length} item left </small></div>
-            <div className="task2 card"></div>
-            <div className="task3 card"></div>
-            <div className="task4 card"></div>
-        </div>
-	);
+            <div className="d-flex justify-content-center flex-column">
+                <ul className="list-group">
+                    {task}
+                </ul>
+                <div className="task1 card card-body"><small class="text-body-secondary"> {taskList.length} item left </small></div>
+                <div className="task2 card"></div>
+                <div className="task3 card"></div>
+                <div className="task4 card"></div>
+            </div>
+        </>
+    );
 };
